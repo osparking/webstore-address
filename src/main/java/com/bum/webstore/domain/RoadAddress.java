5290548c5b4340a1dc089b1f39487430ec01dba2
osparking <jbpark03@gmail.com>
2023-01-02 13:43:00 +0900

    address feature adding steps explained

--- src/main/java/com/bum/webstore/domain/RoadAddress.java
+++ src/main/java/com/bum/webstore/domain/RoadAddress.java
@@ -14,7 +14,7 @@ public class RoadAddress {
 	private String newZipcode;
 	private String roadName;
 	private String detailAddr;
-	
+
 	public String getMgmtNumber() {
 		return mgmtNumber;
 	}
@@ -26,28 +26,21 @@ public class RoadAddress {
 	public String getNewZipcode() {
 		return newZipcode;
 	}
+
 	public void setNewZipcode(String newZipcode) {
 		this.newZipcode = newZipcode;
 	}
+
 	public String getRoadName() {
 		return roadName;
 	}
+
 	public void setRoadName(String roadName) {
 		this.roadName = roadName;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "(우편번호:" + newZipcode + ") " + roadName + ", " + detailAddr;
-	}
-	/**
-	 * 자동 생성된 것임.
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((mgmtNumber == null) ? 0 : mgmtNumber.hashCode());
-		return result;
 	}
 }
